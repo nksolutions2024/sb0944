2a60a36e08d15103850bed151d4d9c108f426b22
sandesh_c12 <sdsoni2042@gmail.com>
2025-01-06 09:59:52 +0530

    8080/abc then Hello

--- src/main/java/com/edigest/myFirstProject1/MyClass.java
+++ src/main/java/com/edigest/myFirstProject1/MyClass.java
@@ -1,4 +1,13 @@
 package com.edigest.myFirstProject1;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyClass {
+
+    @GetMapping("abc")
+    public String sayHello(){
+        return "Hello";
+    }
 }
