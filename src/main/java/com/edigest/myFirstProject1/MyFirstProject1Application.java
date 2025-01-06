0f247b918c72415760ab6d71d0cf407b076dd5a2
sandesh_c12 <sdsoni2042@gmail.com>
2025-01-06 09:46:14 +0530

    spring boot

---
+++ src/main/java/com/edigest/myFirstProject1/MyFirstProject1Application.java
@@ -0,0 +1,13 @@
+package com.edigest.myFirstProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/edigest/myFirstProject1/MyFirstProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edigest.myFirstProject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
